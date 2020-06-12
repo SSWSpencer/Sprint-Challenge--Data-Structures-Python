e48e7a4eadab46dbc9eac556779654558d251b4b
Steven Spencer <steve35spencer@gmail.com>
2020-06-12 11:53:16 -0400

    mvp/stretch

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+import collections
 
 start_time = time.time()
 
@@ -13,11 +14,56 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
 
+
+
+# ========== Original Solution ==========
+
+# class BSTNode:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
+
+#     def insert(self, value):
+#         if self is None:
+#             self = BSTNode(value)
+#         else:
+#             if value < self.value:
+#                 if self.left is None:
+#                     self.left = BSTNode(value)
+#                 else:
+#                     self.left.insert(value)
+#             else:
+#                 if(value == self.value):
+#                     duplicates.append(value)
+#                 if self.right is None:
+#                     self.right = BSTNode(value)
+#                 else:
+#                     self.right.insert(value)
+
+# bst = BSTNode("")
+# for name_1 in names_1:
+#     bst.insert(name_1)
+# for name_2 in names_2:
+#     bst.insert(name_2)
+
+
+
+
+# ========== Stretch Solution ==========
+
+biglist = []
+for x in names_1:
+    biglist.append(x)
+for x in names_2:
+    biglist.append(x)
+duplicates = ([item for item, count in collections.Counter(biglist).items() if count > 1])
+
+
+# Original Runtime: 6.978585720062256 seconds
+# Improved Runtime: 0.10893392562866211 seconds
+# Stretch runtime: 0.008994340896606445 seconds
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
@@ -26,3 +72,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
